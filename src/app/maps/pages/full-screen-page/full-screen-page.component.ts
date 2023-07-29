e1d5098d1c4ec89d6f95170f3e69ae950ae41d8a
santiagofer4 <santiagofernandezdelasierra@gmail.com>
2023-07-28 23:17:00 -0300

    section 286 finished

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+
+import * as mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+ 
+(mapboxgl as any).accessToken = 'pk.eyJ1Ijoic2FudGlhZ29mZXJuMSIsImEiOiJjbDEybTR3NXkxbHNuM3FvMHF2ZDVkamVqIn0.i_6y6Cx7c8Np0YqJE9AURQ';
+
 
 @Component({
   templateUrl: './full-screen-page.component.html',
   styleUrls: ['./full-screen-page.component.scss']
 })
-export class FullScreenPageComponent {
+export class FullScreenPageComponent implements AfterViewInit {
 
-}
+
+  ngAfterViewInit(): void {
+    const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+      });
+  }
+
+  
+} 
